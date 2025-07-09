87866adc9cdf5a2bd06dc774b491c6b9a130297b
sebarosales5 <seba.5.rosales.0.sr@gmail.com>
2025-07-08 23:38:36 -0300

    Algunas mejoras

--- ProyectoFinalPagina/FrontEnd/index.php
+++ ProyectoFinalPagina/FrontEnd/index.php
@@ -54,21 +54,21 @@
     <div class="carousel-item active">
       <img src="../Otros/fotos/draftosauruslogo.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+        <h5 style="color: var(--bs-emphasis-color) !important;">¡Bienvenido a Draftosaurus!</h5>
+        <p style="color: var(--bs-primary);">El juego más divertido de dinosaurios.</p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="../Otros/fotos/Logo_nuevo.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
+        <h5 class="titulo-carousel">¡Conoce del juego!</h5>
+       <!-- <p>Some representative placeholder content for the second slide.</p>-->
       </div>
     </div>
     <div class="carousel-item">
       <img src="../Otros/fotos/pasto1.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
+        <h5 style="color: var(--bs-body-color);">Detalles de los desarrolladores</h5>
         <p>Some representative placeholder content for the third slide.</p>
       </div>
     </div>
@@ -84,13 +84,13 @@
 </div>
 
 <!-- botones -->
-<div class="container d-flex justify-content-center gap-3 mt-4">
+<div class="container d-flex justify-content-center gap-3 mt-5">
   <button type="button" class="btn btn-primary btn-lg">Jugar</button>
   <button type="button" class="btn btn-secondary btn-lg">Puntuación</button>  
 </div>
 
 <!-- Datos -->
-  <div class="container mt-5 mb-5 bg-light text-black border border-3 border-danger">
+  <div class="container mt-5 mb-5 bg-light text-black border border-3 border-secondary">
     <div class="row">
       <div class="col-sm-7 py-4 px-3 col-12"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci rem blanditiis dolores neque expedita distinctio at consectetur similique, iste soluta aliquid quia magnam, numquam eius qui molestiae et veritatis incidunt.</div>
       <div class="col-sm-5 py-4 px-3 col-12"> Lorem ipsum dolor sit amet consectetur adipisicing elit. In, aperiam! Ipsa suscipit nesciunt, nam perferendis quos quidem recusandae repellendus quibusdam numquam provident hic corporis expedita laborum quam qui sed laudantium!</div>
@@ -101,10 +101,66 @@
 <div class="container px-4">
   <div class="row gx-5">
     <div class="col-sm-6 col-12">
-     <div class="p-3 border bg-light">Custom column padding</div>
+     <div class="p-3 border bg-light border-3 border-secondary">
+      <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Estadisticas</h5>
+            <p class="card-text"></p>
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+          </div>
+          <img src="../Otros/fotos/pasto1.jpg" class="card-img-bottom" alt="...">
+        </div>
+     </div>
     </div>
     <div class="col-sm-6 col-12">
-      <div class="p-3 border bg-light">Custom column padding</div>
+      <div class="p-3 border bg-light border-3 border-secondary">
+        <div class="d-grid gap-2">
+          <button class="btn btn-primary" type="button">Button</button>
+          <button class="btn btn-primary" type="button">Button</button>
+          <button class="btn btn-primary" type="button">Button</button>
+        </div>
+       <!-- ACORDEON -->
+        <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the first item’s accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the second item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the third item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+</div>
+            </div>
+          </div>
+      </div>
     </div>
   </div>
 </div>
